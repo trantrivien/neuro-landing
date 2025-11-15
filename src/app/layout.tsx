c139d5c4e5c24ed36421bac6a51ee74044c9d51f
Tran Tri Vien <mr.betran.qn@gmail.com>
2025-11-15 17:02:32 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.className}>
-      <body>
+      <body className=" overflow-x-hidden w-screen max-w-screen">
         <Header />
         {children}
         <Footer />
